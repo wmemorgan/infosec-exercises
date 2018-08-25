e1af9b24bff49b86a9b9516255fc1beeae983f95
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-08-25 12:30:07 +0800

    Post commit test script test #3 [if statement]

--- myApp.js
+++ myApp.js
@@ -96,7 +96,7 @@ app.use(helmet.noSniff())
 // Use `helmet.ieNoOpen()`
 app.use(helmet.ieNoOpen())
 
-console.log(`Yes I can hear you!`)
+console.log(`Let's see if it still works`)
 
 /**  7) Ask browsers to access your site via HTTPS only - `helmet.hsts()` */
 
