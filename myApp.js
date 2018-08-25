0d750aab70cdfcc70c39b722a746000ef825c189
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-08-25 13:58:13 +0800

    Activate helmet.hsts()

--- myApp.js
+++ myApp.js
@@ -113,6 +113,7 @@ app.use(helmet.ieNoOpen())
 // policy we will intercept and restore the header, after inspecting it for testing.
 
 var ninetyDaysInMilliseconds = 90*24*60*60*1000;
+app.use(helmet.hsts({maxAge: ninetyDaysInMilliseconds, force: true}))
 
 
 //**Note**:
