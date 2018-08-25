0b1c729d9a3c82ad12604e32ffcbe0eaa1b2b157
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-08-25 15:01:30 +0800

    Remove single quotes '' from scriptSrc directive

--- myApp.js
+++ myApp.js
@@ -178,7 +178,7 @@ app.use(helmet.noCache())
 app.use(helmet.contentSecurityPolicy({
     directives: {
       defaultSrc: ["'self'"],
-      scriptSrc: ["'self'", "'trusted-cdn.com'"]
+      scriptSrc: ["'self'", "trusted-cdn.com"]
     }
   })
 )
